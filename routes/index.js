79c7c31a5073662d6536c18ac6ac7a13fc6da7b2
huak <komsark_@hotmail.co.th>
2021-02-05 02:28:12 +0700

    commit

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express"),
   passport = require("passport");
 router = express.Router();
 
-router.get("/googlemap", function (req, res) {
+router.get("/", function (req, res) {
   res.render("googleshow");
 });
 module.exports = router;
